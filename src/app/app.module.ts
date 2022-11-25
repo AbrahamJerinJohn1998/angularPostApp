cc93d14ed279bf15805446acc4f520d018dd3bdc
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-25 15:42:44 +0530

    view posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddPostsComponent } from './add-posts/add-posts.component';
+import { ViewPostsComponent } from './view-posts/view-posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddPostsComponent
+    AddPostsComponent,
+    ViewPostsComponent
   ],
   imports: [
     BrowserModule,
